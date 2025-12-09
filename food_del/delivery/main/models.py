be308e86831f056e62a9dc0a079963438a7bd51d
mentos <olgrv36@gmail.com>
2025-12-09 16:36:22 +0200

    Model Restaurant added

--- food_del/delivery/main/models.py
+++ food_del/delivery/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model): 
+    name = models.CharField("Name", max_length=50) 
+    address = models.TextField("Address", max_length=100) 
+    def __str__(self): 
+        return self.name 
+    class Meta: 
+        verbose_name = "Restaurant" 
+        verbose_name_plural = "Restaurants"
