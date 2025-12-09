c0eca9e6ab59678430126135ca9058e47b806552
mentos <olgrv36@gmail.com>
2025-12-09 20:03:16 +0200

    all models done, superuser added

--- food_del/delivery/main/admin.py
+++ food_del/delivery/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Restaurant, Dish, Order
 
-# Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Dish)
+admin.site.register(Order)
+
